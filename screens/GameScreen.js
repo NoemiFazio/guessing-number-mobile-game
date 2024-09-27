55b7158cd0db25c54d0dfe60e7b7df69229e241a
Waterlilyrr <noemict94@hotmail.it>
2024-09-27 16:17:08 +0200

    feat(App): implements switch screen logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,1 +1,22 @@
-export default function GameScreen() {}
+import { Text, View, StyleSheet } from "react-native";
+
+export default function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text>Game Screen!</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
