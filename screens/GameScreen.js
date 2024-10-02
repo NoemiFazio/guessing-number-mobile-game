e908631fb3326220b0f7892c941c12a35b2ae67d
Waterlilyrr <noemict94@hotmail.it>
2024-10-02 12:28:51 +0200

    feat(GameScreen): adds nextGuessHandler

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,45 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import { useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
 import generateRandomBetween from "../utils/generateRandomBetween";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
+let minBoundary = 1;
+let maxBoundary = 100;
 
 export default function GameScreen({ userNumber }) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "higher" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "you know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRandomNum = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRandomNum);
+  }
+
   return (
     <View style={styles.screen}>
       <Title text="Opponent's Guess" />
@@ -14,7 +47,14 @@ export default function GameScreen({ userNumber }) {
       {/* GUESS */}
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <View style={styles.buttonsContainer}>
+          <PrimaryButton onPress={() => nextGuessHandler("higher")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
@@ -26,4 +66,5 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
   },
+  buttonsContainer: {},
 });
